86c6f7d0dee5385dd4d45afe3c370fa0655744c9
DAKSH SEMWAL <dsemwal2@gmail.com>
2019-07-09 11:22:41 +0530

    Updates

---
+++ precache-manifest.da6788f6cf9306d54c96dadb10aecd7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "56246f06fe5b0436386e",
+    "url": "/me/static/js/main.56246f06.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "56246f06fe5b0436386e",
+    "url": "/me/static/css/main.54f3f201.chunk.css"
+  },
+  {
+    "revision": "41791cd813092985d2ec31c22cb37a59",
+    "url": "/me/index.html"
+  }
+];
